7f0af4fe3c79e87985c939db9a2c93493f1259b6
Aishwarya Iyer <aradhakrishnaniye@deloitte.com>
2022-09-28 12:11:57 +0530

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Radhakrishnan Iyer, Aishwarya on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

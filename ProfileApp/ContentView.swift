ad8fca851a5f9cfcb89d86a0bd536fc3b2328a08
Aishwarya Iyer <aradhakrishnaniye@deloitte.com>
2022-09-28 19:13:09 +0530

    Profile app design first commit

--- ProfileApp/ContentView.swift
+++ ProfileApp/ContentView.swift
@@ -9,13 +9,112 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       ZStack {
+           Image("background").resizable().edgesIgnoringSafeArea(.all)
+           
+           VStack(alignment: .center) {
+               Spacer()
+               Spacer()
+               VStack(alignment: .center, spacing: 20) {
+                   Image("myImage").resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(width: 180, height: 180, alignment: .top)
+                       .clipShape(Circle())
+                       .shadow(color: .pink, radius: 5, x: 5, y: 5)
+                   
+                   Text("Your Name")
+                       .font(.system(size: 30))
+                       .fontWeight(.bold)
+                       .foregroundColor(.white)
+                       .font(.system(.largeTitle))
+                       .shadow(radius: 5)
+                   
+                   Text("iOS | Front end developer")
+                       .foregroundColor(.white)
+                       .font(.title2)
+                   
+                   HStack(spacing: 40) {
+                       Image(systemName: "heart.circle")
+                           .resizable()
+                           .aspectRatio( contentMode: .fit)
+                       
+                       Image(systemName: "network")
+                           .resizable()
+                           .aspectRatio( contentMode: .fit)
+                       
+                       Image(systemName: "message.circle")
+                           .resizable()
+                           .aspectRatio( contentMode: .fit)
+                       
+                       Image(systemName: "phone.circle")
+                           .resizable()
+                           .aspectRatio( contentMode: .fit)
+                       
+                   }.foregroundColor(.white)
+                    .frame(width: 250, height: 50, alignment: .center)
+                    .shadow(color: .pink, radius: 5, x: 5, y: 8)
+                   
+                   
+               }
+               VStack(alignment: .center,spacing: 30) {
+                   Spacer()
+                   RoundedRectangle(cornerRadius: 120)
+                       .frame(width: 200, height: 50, alignment: .center)
+                       .foregroundColor(.white)
+                       .shadow(color: .pink, radius: 8, y: 8)
+                       .overlay(
+                        Text("Follow")
+                        .fontWeight(.bold)
+                        .foregroundColor(.pink)
+                        .font(.system(size: 20))
+                       )
+                   
+                
+                   HStack(alignment: .center, spacing: 40) {
+                       VStack {
+                           Text("222")
+                               .font(.title)
+                               .foregroundColor(.pink)
+                           Text("Appreciations")
+                               .font(.caption2)
+                               .foregroundColor(.gray)
+                           
+                       }
+                       VStack {
+                           Text("222")
+                               .font(.title)
+                               .foregroundColor(.pink)
+                           Text("Appreciations")
+                               .font(.caption2)
+                               .foregroundColor(.gray)
+                           
+                       }
+                       VStack {
+                           Text("222")
+                               .font(.title)
+                               .foregroundColor(.pink)
+                           Text("Appreciations")
+                               .font(.caption2)
+                               .foregroundColor(.gray)
+                           
+                       }
+                   }
+                   Text("About you")
+                       .font(.system(size: 30))
+                       .font(.system(.largeTitle))
+                   
+                   Text("I'm an iOS Front Developer. Welcome to the series of iOS proejct. Let's create some exciting projects")
+                       .font(.system(.body))
+                       .foregroundColor(.black)
+                       .multilineTextAlignment(.center)
+                       .lineLimit(4)
+                       .opacity(0.5)
+                
+               }.padding(10)
+               Spacer()
+            
+           }
         }
-        .padding()
     }
 }
 
